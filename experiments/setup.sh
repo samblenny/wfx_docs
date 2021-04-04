52553c2c27034a35b529b064db8efbc157507c76
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2021-04-04 11:28:39 -0500

    setup script for wfx driver

--- experiments/setup.sh
+++ experiments/setup.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # Prepare the wfx_fmac_driver library directory
 git clone --depth 2 https://github.com/samblenny/wfx-fullMAC-driver.git -b include-paths
-cd wfx-fullMAC-driver
 ln -s wfx-fullMAC-driver/wfx_fmac_driver wfx_fmac_driver
