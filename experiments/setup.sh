502f2a721de4b5bae1b3253a6e2de3d09bcc25c3
Sam Blenny <68084116+samblenny@users.noreply.github.com>
2021-04-23 09:58:50 +0000

    add method to get BRD8022A PDS file

--- experiments/setup.sh
+++ experiments/setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 # Prepare the wfx_fmac_driver library directory
-git clone --depth 2 https://github.com/samblenny/wfx-fullMAC-driver.git -b include-paths
+git clone https://github.com/samblenny/wfx-fullMAC-driver.git -b zero-buf
 ln -s wfx-fullMAC-driver/wfx_fmac_driver wfx_fmac_driver
+mkdir -p wfx-fullMAC-tools/PDS
+cd wfx-fullMAC-tools/PDS
+curl -O https://raw.githubusercontent.com/SiliconLabs/wfx-fullMAC-tools/master/PDS/brd8022a_pds.h
+cd ../..
